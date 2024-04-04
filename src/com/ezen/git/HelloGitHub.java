9ad0c9232120e589d0dd7ca95c815d204588608e
bangry313 <bangry313@gmail.com>
2024-04-04 17:24:23 +0900

    테스트 커밋

--- src/com/ezen/git/HelloGitHub.java
+++ src/com/ezen/git/HelloGitHub.java
@@ -6,5 +6,6 @@ public class HelloGitHub {
         System.out.println("2222222222222");
         // 집에서 과제 수행
         System.out.println("33333333333333");
+        System.out.println("4444444444444444");
     }
 }
